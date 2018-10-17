e85d06c06c3753deb47d9877e2291c72594e9e43
byrne-saw <byrnes.aw@gmail.com>
2018-10-17 17:58:35 -0500

    finishing class exercise

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo App</h1>
+    <div>
+      <h3>New Task</h3>
+      <div>
+        <input v-model="newTask.text">
+        <button v-on:click="addTask()">Add Task</button>
+      </div>
+    </div>
+    <div>
+      <h5>{{ numberOfIncompleteTasks() }} tasks left</h5>
+    </div>
+    <ul>
+      <div v-for="task in tasks" v-bind:class="{strike: task.completed}">
+        <li @click="completeTask(task)">{{ task.text }}</li>
+      </div>  
+    </ul>
   </div>
 </template>
 
+<style>
+.strike {
+  text-decoration: line-through;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      tasks: [],
+      newTask: {text: '', completed: false}
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/tasks")
+    .then(function(response) {
+      this.tasks = response.data;
+    }.bind(this));
+  },
+  methods: {
+    addTask: function() {
+      if (this.newTask.text !== "") {
+        this.tasks.push(this.newTask);
+        this.newTask = {text: "", completed: false};
+      }
+    },
+    completeTask: function(inputTask) {
+      inputTask.completed = !inputTask.completed;
+    },
+    numberOfIncompleteTasks: function() {
+      var count = 0;
+      this.tasks.forEach(function(task) {
+        if (!task.completed) {
+          count++;
+        }
+      });
+      return count;
+    },
+    removeCompelete: function() {
+
+    }
+  },
+  computed: {}
+};
 </script>
+
+
+
+
+
+
+
+
+
+
